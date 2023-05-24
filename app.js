f290c57f6d176d908298a9baf249061ff074e897
Zach Ruiz <powersbooth10@gmail.com>
2023-05-24 13:45:44 -0500

    passed challenge 4 and 5.

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ app.get('/', (req, res) => {
   res.render('home', {startingContent: homeStartingContent})
 });
 
+app.get('/about', (req, res) => {
+  res.render('about', {aboutContent: aboutContent})
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {contactContent: contactContent})
+});
+
 
 
 
